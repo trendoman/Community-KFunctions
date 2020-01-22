9a8478333d50413edccfc925d91107a54f29c38c
Anton Smirnov <tony.smirnov@gmail.com>
2020-01-22 22:16:09 +0300

    Add files via upload

---
+++ kfunctions.community.php
@@ -0,0 +1,35 @@
+<?php
+if ( !defined('K_COUCH_DIR') ) die(); // cannot be loaded directly
+
+/**
+*    Default list of CouchCMS addons
+*    From file 'addons/kfunctions.example.php':
+*/
+
+//require_once( K_COUCH_DIR.'addons/cart/cart.php' ); //Shopping Cart @ https://docs.couchcms.com/concepts/shopping-cart-1.html https://docs.couchcms.com/concepts/shopping-cart-2.html
+//require_once( K_COUCH_DIR.'addons/inline/inline.php' ); //On Page Editing @ https://docs.couchcms.com/concepts/on-page-editing.html
+//require_once( K_COUCH_DIR.'addons/extended/extended-users.php' ); //Extended Users @ https://www.couchcms.com/docs/extended-entities/post.htm
+//require_once( K_COUCH_DIR.'addons/extended/extended-folders.php' ); //Extended Folders @ https://www.couchcms.com/docs/extended-entities/post2.htm
+//require_once( K_COUCH_DIR.'addons/extended/extended-comments.php' ); //Extended Comments @ https://www.couchcms.com/docs/extended-entities/post3.htm
+//require_once( K_COUCH_DIR.'addons/routes/routes.php' ); //Custom Routes @ https://www.couchcms.com/docs/custom-routes/
+//require_once( K_COUCH_DIR.'addons/jcropthumb/jcropthumb.php' ); //ADDON: Advanced thumbnail cropping @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=9299
+
+
+/**
+*    More addons for CouchCMS
+*    Usually coming from Tips&Tricks @ https://www.couchcms.com/forum/viewforum.php?f=8
+*/
+
+//require_once( K_COUCH_DIR.'addons/bootstrap-grid/bootstrap-grid.php' ); //ADDON: Bootstrap grid in admin panel @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=11023
+//require_once( K_COUCH_DIR.'addons/copy-to-new/copy-to-new.php' );    //Copy an existing page to create a new page @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=11545
+//require_once( K_COUCH_DIR.'addons/csv/csv.php' ); //CSV Importer Module @ https://www.couchcms.com/forum/viewtopic.php?f=5&t=8803 https://github.com/CouchCMS/CSV
+//require_once( K_COUCH_DIR.'addons/filter/filter.php' ); //In AdminPanel List View: Filter pages by a Custom Field @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=10594&p=26489&view=show#p27057
+//require_once( K_COUCH_DIR.'addons/grayscale/grayscale.php' ); //ADDON: Greyscaling images @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=8148
+//require_once( K_COUCH_DIR.'addons/minify-js-css/minify.php' ); //Combine and Minimize JS and CSS Files @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=11090
+//require_once( K_COUCH_DIR.'addons/multi-lang/multi-lang.php' ); // Addon for multi-lingual sites @ https://couchcms.com/forum/viewtopic.php?f=5&t=10979
+//require_once( K_COUCH_DIR.'addons/simple-access-control/simple-access-control.php' ); //Restrict access to specific portions of the admin-panel to certain admins @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=10715#p27178
+//require_once( K_COUCH_DIR.'addons/tweets/tweets.php' ); //ADDON: Latest Tweets @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=7730 https://github.com/CouchCMS/Tweets
+//require_once( K_COUCH_DIR.'addons/uid/uid.php' ); //ADDON: Sequential ID @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=11372
+//require_once( K_COUCH_DIR.'addons/votes/votes.php' ); //Votes, Ratings & Polls module @ https://www.couchcms.com/forum/viewtopic.php?f=5&t=8133 https://github.com/CouchCMS/Votes
+//require_once( K_COUCH_DIR.'addons/watermark/watermark.php' ); //ADDON: Watermarking images @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=8012 https://github.com/CouchCMS/Watermark
+//$FUNCS->register_admin_listview( 'comments.php', 'comments-admin-list.php' ); // Threaded Comments Implementation @ https://www.couchcms.com/forum/viewtopic.php?f=8&t=9182
